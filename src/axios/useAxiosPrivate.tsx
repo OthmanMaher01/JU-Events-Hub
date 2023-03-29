c886640a37e20c814425bc8ba523d76a6d794956
hamza alali <hamza.tech.alali@gmail.com>
2023-03-29 19:50:46 +0300

    changed base url , added api prefix

--- src/axios/useAxiosPrivate.tsx
+++ src/axios/useAxiosPrivate.tsx
@@ -1,7 +1,7 @@
 import RefreshRequest from "./RefreshRequest"
 import axios from "axios";
 export const axiosPrivate = axios.create({
-    baseURL : 'http://127.0.0.1:8080',
+    baseURL : 'http://127.0.0.1:8080/api/',
     headers:{
       "Content-Type":"application/json",
       "Access-Control-Allow-Origin": "*",
